f867a4917215173e9e0262ffeeadf53453bd90c9
fabulouschase <belloabdulrahmon345@gmail.com>
2025-09-16 11:45:25 +0100

    resetpassword models

--- jelibit/models.py
+++ jelibit/models.py
@@ -6,6 +6,9 @@ from datetime import date
 from django.utils import timezone
 from datetime import timedelta
 from django.core.mail import send_mail
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 
 class CustomUserManager(BaseUserManager):
@@ -61,7 +64,21 @@ class EmailVerificationCode(models.Model):
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
     
 
-  
+
+
+# yourapp/models.py
+
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"Token for {self.user.email}"
+
+
 
 
 
